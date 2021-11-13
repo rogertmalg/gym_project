cae0d5b58238faac4fc19575e3b774b575d14d92
rogertmalg <roger.malgueira@outlook.com>
2021-11-13 15:49:24 +0000

    adds activity repo functions

--- console.py
+++ console.py
@@ -1,7 +1,7 @@
 import pdb
 
-# from models.activity import Activity
-# import repositories.activity_repository as activity_repository
+from models.activity import Activity
+import repositories.activity_repository as activity_repository
 
 # from models.booking import Booking
 # import repositories.booking_repository as booking_repository
@@ -22,27 +22,19 @@ instructor_repository.save(instructor_1)
 instructor_2 = Instructor("Carla", "I like lifiting weights")
 instructor_repository.save(instructor_2)
 
-instructor_1.name = "Jessica"
-instructor_1.bio = "I like running"
-instructor_repository.update(instructor_1)
+activity_1 = Activity('Yoga', instructor_1, 'studio 03', 20, '2021-11-25', '11:00', True)
+activity_repository.save(activity_1)
 
+activity_1.name = "Hit"
+activity_1.instructor = instructor_2
+activity_repository.update(activity_1)
 
-instructors = instructor_repository.select_all()
-for instructor in instructors:
-    print(instructor.__dict__)
+# activities = activity_repository.select_all()
+# for activity in activities:
+#     print(activity.__dict__)
 
+# activity_repository.delete(1)
 
-
-
-# 
-
-
-
-
-
-
-
-
-
+# print(activity_repository.select(2))
 
 pdb.set_trace()
--- repositories/activity_repository.py
+++ repositories/activity_repository.py
@@ -1,1 +1,60 @@
 from db.run_sql import run_sql
+from models.member import Member
+from models.activity import Activity
+from models.instructor import Instructor
+import repositories.instructor_repository as instructor_repository
+
+def save(activity):
+    sql = "INSERT INTO activities (name, instructor_id, room, capacity, date, time, active ) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING id"
+    values = [activity.name, activity.instructor.id, activity.room, activity.capacity, activity.date, activity.time, activity.active]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    activity.id = id
+
+
+def select_all():
+    activities = []
+    sql = "SELECT * FROM activities"
+    results = run_sql(sql)
+    for result in results:
+        instructor = instructor_repository.select(result["instructor_id"])
+        activity = Activity(result["name"], instructor, result["room"], result["capacity"], result["date"], result["time"], result["active"], result["id"])
+        activities.append(activity)
+    return activities
+
+
+def select(id):
+    sql = "SELECT * FROM activities WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    instructor = instructor_repository.select(result["instructor_id"])
+    activity = Activity(result["name"], instructor, result["room"], result["capacity"], result["date"], result["time"], result["active"], result["id"])
+    return activity
+
+
+def delete_all():
+    sql = "DELETE FROM activities"
+    run_sql(sql)
+
+
+def delete(id):
+    sql = "DELETE FROM activities WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+
+def update(activity):
+    sql = "UPDATE activities SET (name, instructor_id, room, capacity, date, time, active) = (%s, %s, %s, %s, %s, %s, %s) WHERE id = %s"
+    values = [activity.name, activity.instructor.id, activity.room, activity.capacity, activity.date, activity.time, activity.active, activity.id]
+    run_sql(sql, values)
+
+
+def select_members_in_activity(id):
+    attending = []
+    sql = "SELECT member.* FROM members INNER JOIN bookings ON bookings.member_id = member.id WHERE bookings.activity_id = %s"
+    values = [id]
+    results = run_sql(sql, values)
+    for result in results:
+        member = Member(result["name"], result["premium"], result["active"], result["id"])
+        attending.append(member)
+    return attending
