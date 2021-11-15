ba3c29f0944625d91157dd91605149ffb7e31f49
rogertmalg <roger.malgueira@outlook.com>
2021-11-15 12:37:28 +0000

    members controller and templates working

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from controllers.instructors_controller import instructors_blueprint
+from controllers.members_controller import members_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(instructors_blueprint)
+app.register_blueprint(members_blueprint)
 
 @app.route("/")
 def main():
---
+++ controllers/members_controller.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, Flask, redirect, render_template, request 
+
+from models.member import Member
+import repositories.member_repository as member_repository
+
+members_blueprint = Blueprint("members", __name__)
+
+# index
+@members_blueprint.route("/members")
+def members():
+    all_members = member_repository.select_all()
+    return render_template("members/index.html", all_members = all_members)
+
+# new - form
+@members_blueprint.route("/members/new")
+def new_member():
+    return render_template("members/new.html")
+
+# create - post 
+@members_blueprint.route("/members", methods=["POST"])
+def create_member():
+    name = request.form["name"]
+    premium = request.form["premium"]
+    active = request.form["active"]
+    new_member = Member(name, premium, active)
+    member_repository.save(new_member)
+    return redirect("/members")
+
+# Edit
+@members_blueprint.route("/members/<id>/edit")
+def edit_member(id):
+    member = member_repository.select(id)
+    return render_template("members/edit.html", member = member)
+
+# Update
+@members_blueprint.route("/members/<id>", methods=["POST"])
+def update_member(id):
+    name = request.form["name"]
+    premium = request.form["premium"]
+    active = request.form["active"]
+    member = Member(name, premium, active, id)
+    member_repository.update(member)
+    return redirect("/members")
+
+# Delete 
+@members_blueprint.route("/members/<id>/delete", methods=["POST"])
+def delete_member(id):
+    member_repository.delete(id)
+    return redirect("/members")
